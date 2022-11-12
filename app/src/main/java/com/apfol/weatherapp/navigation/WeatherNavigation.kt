564c7fb915d69ddeabf191dec38b93b72c539d5f
AndresFab <andresf.ramos@mercadolibre.com.co>
2022-11-12 08:34:08 -0500

    Update WeatherNavigation.kt

--- app/src/main/java/com/apfol/weatherapp/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/apfol/weatherapp/navigation/WeatherNavigation.kt
@@ -4,14 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.apfol.weatherapp.screens.splash.WeatherSearchScreen
+import com.apfol.weatherapp.screens.search.WeatherSearchScreen
 import com.apfol.weatherapp.screens.splash.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController,
-        startDestination = WeatherScreens.SplashScreen.name ) {
+    NavHost(
+        navController = navController,
+        startDestination = WeatherScreens.SplashScreen.name
+    ) {
         composable(WeatherScreens.SplashScreen.name){
             WeatherSplashScreen(navController = navController)
         }
