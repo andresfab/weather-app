1c39d48a877889c9a76e27dafd40828dbb84bd34
AndresFab <andresf.ramos@mercadolibre.com.co>
2022-11-13 11:30:52 -0500

    Update WeatherSearchScreen.kt

--- app/src/main/java/com/apfol/weatherapp/screens/search/WeatherSearchScreen.kt
+++ app/src/main/java/com/apfol/weatherapp/screens/search/WeatherSearchScreen.kt
@@ -1,6 +1,5 @@
 package com.apfol.weatherapp.screens.search
 
-import android.widget.Space
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -23,12 +21,10 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.apfol.weatherapp.domain.model.WeatherSearchResult
